5d0d5c8d2180cb8e42c46e9b00eb1f3bd2aac7d5
crestavo <muhammadyusufrizkie@gmail.com>
2025-06-23 23:25:07 +0700

    Add split window keymap

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -8,10 +8,10 @@ vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- Alt Up/Down in vscode
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mzJ`z") -- Remap joining lines
+vim.keymap.set("n", "J", "mzJ`z")       -- Remap joining lines
 vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Keep cursor in place while moving up/down page
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv") -- center screen when looping search results
+vim.keymap.set("n", "n", "nzzzv")       -- center screen when looping search results
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- paste and don't replace clipboard over deleted text
@@ -53,21 +53,43 @@ vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 -- source file
 vim.keymap.set("n", "<leader><leader>", function()
-	vim.cmd("so")
+  vim.cmd("so")
 end)
+
+-- Set leader key ke <Space>
+vim.g.mapleader = " "
+
+-- Split horizontal dan vertical
+vim.keymap.set("n", "<leader>sh", ":split<CR>")
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>")
+
+-- Navigasi antar split
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+
+-- Resize window
+vim.keymap.set("n", "<C-Down>", ":resize -5<CR>")
+vim.keymap.set("n", "<C-Up>", ":resize +5<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize -5<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +5<CR>")
+
+-- Menutup window aktif
+vim.keymap.set("n", "<leader>q", ":q<CR>")
 
 -- Toggle diagnostic
 local diagnostics_active = true
 -- function toggle diagnostic
 local function toggle_diagnostics()
-	diagnostics_active = not diagnostics_active
-	if diagnostics_active then
-		vim.diagnostic.enable()
-		print("Diagnostics enabled")
-	else
-		vim.diagnostic.disable()
-		print("Diagnostics disabled")
-	end
+  diagnostics_active = not diagnostics_active
+  if diagnostics_active then
+    vim.diagnostic.enable()
+    print("Diagnostics enabled")
+  else
+    vim.diagnostic.disable()
+    print("Diagnostics disabled")
+  end
 end
 -- Keymap toggle
 vim.keymap.set("n", "<leader>ud", toggle_diagnostics, { noremap = true, silent = true })
