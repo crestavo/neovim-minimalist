c4f9bfafaa6aa6c1034353728f39bdbcad45eceb
crestavo <muhammadyusufrizkie@gmail.com>
2025-07-19 19:32:06 +0700

    fix(keybind): change keybind for opening explorer

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,6 +1,6 @@
 -- KEYBINDS
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
